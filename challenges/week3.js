b66dca4f7c21426ed4632528349ffa901e14d4e8
Alba Rodriguez <albarodriguezgalvez@gmail.com>
2020-03-02 17:09:54 +0000

    implemented checkIngredients function

--- challenges/week3.js
+++ challenges/week3.js
@@ -6,21 +6,27 @@ function getSquares(nums) {
   return square
 }
 
-function camelCaseWords(words) {
+function camelCaseWords(words) { //"my", "variable"
   if (words === undefined) throw new Error("words is required");
-  // Your code here!
+
 }
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
 }
 
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
+  for (let i = 0; i < menu.length; i++) {
+    for (ing in menu) {
+      if (menu[i]['ingredients'].includes(ingredient)) {
+        return true
+      }
+    }
+  } return false
 }
+
 
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
