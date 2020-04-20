d4b71d03bda376e22606d5d76afe6f585a9c3102
Alba Rodriguez <albarodriguezgalvez@gmail.com>
2020-04-20 12:10:46 +0100

    Implemented test and function for getScreentimeAlertList

--- challenges/week10.js
+++ challenges/week10.js
@@ -33,72 +33,85 @@ const createRange = (start, end, step) => {
   }
 }
 
-  /**
-   * This function takes an array of user objects and their usage in minutes of various applications. The format of the data should be as follows:
-   * [
-   *  {
-   *    username: "beth_1234",
-   *    name: "Beth Smith",
-   *    screenTime: [
-   *                 { date: "2019-05-01", usage: { twitter: 34, instagram: 22, facebook: 40} },
-   *                 { date: "2019-05-02", usage: { twitter: 56, instagram: 40, facebook: 31} },
-   *                 { date: "2019-05-03", usage: { twitter: 12, instagram: 15, facebook: 19} },
-   *                 { date: "2019-05-04", usage: { twitter: 10, instagram: 56, facebook: 61} },
-   *                ]
-   *   },
-   *   {
-   *    username: "sam_j_1989",
-   *    name: "Sam Jones",
-   *    screenTime: [
-   *                 { date: "2019-06-11", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 10} },
-   *                 { date: "2019-06-13", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 16} },
-   *                 { date: "2019-06-14", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 31} },
-   *                ]
-   *   },
-   * ]
-   *
-   * The function should return an array of usernames of users who have used more than 100 minutes of screentime for a given date.
-   * The date will be provided in the format "2019-05-04" (YYYY-MM-DD)
-   * For example, if passed the above users and the date "2019-05-04" the function should return ["beth_1234"] as she used over 100 minutes of screentime on that date.
-   * @param {Array} users
-   */
-  const getScreentimeAlertList = (users, date) => {
-    if (users === undefined) throw new Error("users is required");
-    if (date === undefined) throw new Error("date is required");
-  };
+/**
+ * This function takes an array of user objects and their usage in minutes of various applications. The format of the data should be as follows:
+ * [
+ *  {
+ *    username: "beth_1234",
+ *    name: "Beth Smith",
+ *    screenTime: [
+ *                 { date: "2019-05-01", usage: { twitter: 34, instagram: 22, facebook: 40} },
+ *                 { date: "2019-05-02", usage: { twitter: 56, instagram: 40, facebook: 31} },
+ *                 { date: "2019-05-03", usage: { twitter: 12, instagram: 15, facebook: 19} },
+ *                 { date: "2019-05-04", usage: { twitter: 10, instagram: 56, facebook: 61} },
+ *                ]
+ *   },
+ *   {
+ *    username: "sam_j_1989",
+ *    name: "Sam Jones",
+ *    screenTime: [
+ *                 { date: "2019-06-11", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 10} },
+ *                 { date: "2019-06-13", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 16} },
+ *                 { date: "2019-06-14", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 31} },
+ *                ]
+ *   },
+ * ]
+ *
+ * The function should return an array of usernames of users who have used more than 100 minutes of screentime for a given date.
+ * The date will be provided in the format "2019-05-04" (YYYY-MM-DD)
+ * For example, if passed the above users and the date "2019-05-04" the function should return ["beth_1234"] as she used over 100 minutes of screentime on that date.
+ * @param {Array} users
+ */
+const getScreentimeAlertList = (users, date) => {
+  if (users === undefined) throw new Error("users is required");
+  if (date === undefined) throw new Error("date is required");
+  const values = Object.values(users)
+  let minutes = 0
+  for (i = 0; i < values.length; i++) {
+    for (j = 0; j < values[i]['screenTime'].length; j++) {
+      if (values[i]['screenTime'][j]['date'] === date) {
+        for (key in values[i]['screenTime'][j]['usage'])
+          minutes = minutes + values[i]['screenTime'][j]['usage'][key]
+      }
+    }
+    if (minutes > 100) {
+      return values[i]['username']
+    } 
+  } return "0 results"
+}
 
-  /**
-   * This function will receive a hexadecimal color code in the format #FF1133. A hexadecimal code is a number written in hexadecimal notation, i.e. base 16. If you want to know more about hexadecimal notation:
-   * https://www.youtube.com/watch?v=u_atXp-NF6w
-   * For colour codes, the first 2 chars (FF in this case) represent the amount of red, the next 2 chars (11) represent the amound of green, and the last 2 chars (33) represent the amount of blue.
-   * Colours can also be represented in RGB format, using decimal notation.
-   * This function should transform the hex code into an RGB code in the format:
-   * "rgb(255,17,51)"
-   * Hint: You will need to convert each hexadecimal value for R, G and B into its decimal equivalent!
-   * @param {String} str
-   */
-  const hexToRGB = hexStr => {
-    if (hexStr === undefined) throw new Error("hexStr is required");
-  };
+/**
+ * This function will receive a hexadecimal color code in the format #FF1133. A hexadecimal code is a number written in hexadecimal notation, i.e. base 16. If you want to know more about hexadecimal notation:
+ * https://www.youtube.com/watch?v=u_atXp-NF6w
+ * For colour codes, the first 2 chars (FF in this case) represent the amount of red, the next 2 chars (11) represent the amound of green, and the last 2 chars (33) represent the amount of blue.
+ * Colours can also be represented in RGB format, using decimal notation.
+ * This function should transform the hex code into an RGB code in the format:
+ * "rgb(255,17,51)"
+ * Hint: You will need to convert each hexadecimal value for R, G and B into its decimal equivalent!
+ * @param {String} str
+ */
+const hexToRGB = hexStr => {
+  if (hexStr === undefined) throw new Error("hexStr is required");
+};
 
-  /**
-   * This function takes a noughts and crosses board represented as an array, where an empty space is represented with null.
-   * [
-   *  ["X", "0", null],
-   *  ["X", null, "0"],
-   *  ["X", null, "0"]
-   * ]
-   * The function should return "X" if player X has won, "0" if the player 0 has won, and null if there is currently no winner.
-   * @param {Array} board
-   */
-  const findWinner = board => {
-    if (board === undefined) throw new Error("board is required");
-  };
+/**
+ * This function takes a noughts and crosses board represented as an array, where an empty space is represented with null.
+ * [
+ *  ["X", "0", null],
+ *  ["X", null, "0"],
+ *  ["X", null, "0"]
+ * ]
+ * The function should return "X" if player X has won, "0" if the player 0 has won, and null if there is currently no winner.
+ * @param {Array} board
+ */
+const findWinner = board => {
+  if (board === undefined) throw new Error("board is required");
+};
 
-  module.exports = {
-    sumDigits,
-    createRange,
-    getScreentimeAlertList,
-    hexToRGB,
-    findWinner
-  };
+module.exports = {
+  sumDigits,
+  createRange,
+  getScreentimeAlertList,
+  hexToRGB,
+  findWinner
+};
--- test/week10.test.js
+++ test/week10.test.js
@@ -1,14 +1,15 @@
 const {
     sumDigits,
     createRange,
-    
+    getScreentimeAlertList
+
 } = require("../challenges/week10")
 
 describe("sumDigits", () => {
     test("it returns the sum of all digits", () => {
         expect(sumDigits(123)).toBe(6),
-        expect(sumDigits(23456)).toBe(20),
-        expect(sumDigits(34)).toBe(7)
+            expect(sumDigits(23456)).toBe(20),
+            expect(sumDigits(34)).toBe(7)
     })
     test("it returns 0 if digit if 0", () => {
         expect(sumDigits(0)).toBe(0)
@@ -19,7 +20,85 @@ describe("createRange", () => {
     test("It returns an array that starts with 3 and ends with 15. The step is 5", () => {
         expect(createRange(3, 23, 5)).toEqual([3, 8, 13, 18, 23])
     }),
-    test("It returns an array that starts with 4 and ends with 20. Step is not provided", () => {
-        expect(createRange(4, 10)).toEqual([4, 5, 6, 7, 8, 9, 10])
+        test("It returns an array that starts with 4 and ends with 20. Step is not provided", () => {
+            expect(createRange(4, 10)).toEqual([4, 5, 6, 7, 8, 9, 10])
+        });
+});
+describe("getScreentimeAlertList", () => {
+    test("It returns an array of usernames of users who have used more than 100 minutes of screentime for a given date", () => {
+        const users = [
+            {
+                username: "beth_1234",
+                name: "Beth Smith",
+                screenTime: [
+                    { date: "2019-05-01", usage: { twitter: 34, instagram: 22, facebook: 40 } },
+                    { date: "2019-05-02", usage: { twitter: 56, instagram: 40, facebook: 31 } },
+                    { date: "2019-05-03", usage: { twitter: 12, instagram: 15, facebook: 19 } },
+                    { date: "2019-05-04", usage: { twitter: 10, instagram: 56, facebook: 61 } },
+                ]
+            },
+            {
+                username: "sam_j_1989",
+                name: "Sam Jones",
+                screenTime: [
+                    { date: "2019-06-11", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 10 } },
+                    { date: "2019-06-13", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 16 } },
+                    { date: "2019-06-14", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 31 } },
+                ]
+            },
+        ];
+        expect(getScreentimeAlertList(users, "2019-05-04")).toBe("beth_1234");
+    });
+    test("It returns an array of usernames of users who have used more than 100 minutes of screentime for a given date", () => {
+        const users = [
+            {
+                username: "beth_1234",
+                name: "Beth Smith",
+                screenTime: [
+                    { date: "2019-05-01", usage: { twitter: 34, instagram: 22, facebook: 40 } },
+                    { date: "2019-06-14", usage: { twitter: 34, instagram: 2, facebook: 40 } },
+                    { date: "2019-05-02", usage: { twitter: 56, instagram: 40, facebook: 31 } },
+                    { date: "2019-05-03", usage: { twitter: 12, instagram: 15, facebook: 19 } },
+                    { date: "2019-05-04", usage: { twitter: 10, instagram: 56, facebook: 61 } },
+                ]
+            },
+            {
+                username: "sam_j_1989",
+                name: "Sam Jones",
+                screenTime: [
+                    { date: "2019-06-11", usage: { mapMyRun: 2, whatsApp: 0, facebook: 0, safari: 10 } },
+                    { date: "2019-06-13", usage: { mapMyRun: 10, whatsApp: 20, facebook: 10, safari: 16 } },
+                    { date: "2019-06-14", usage: { mapMyRun: 42, whatsApp: 20, facebook: 40, safari: 31 } },
+                    { date: "2019-05-04", usage: { twitter: 10, instagram: 56, facebook: 61 } },
+                ]
+            },
+        ];
+        expect(getScreentimeAlertList(users, "2019-06-14")).toBe("sam_j_1989")
+    });
+    test("It returns 0 results if nobody spent over 100 minutes", () => {
+        const users = [
+            {
+                username: "beth_1234",
+                name: "Beth Smith",
+                screenTime: [
+                    { date: "2019-05-01", usage: { twitter: 34, instagram: 22, facebook: 40 } },
+                    { date: "2019-06-14", usage: { twitter: 34, instagram: 2, facebook: 40 } },
+                    { date: "2019-05-02", usage: { twitter: 56, instagram: 40, facebook: 31 } },
+                    { date: "2019-05-03", usage: { twitter: 12, instagram: 15, facebook: 19 } },
+                    { date: "2019-05-04", usage: { twitter: 10, instagram: 56, facebook: 61 } },
+                ]
+            },
+            {
+                username: "sam_j_1989",
+                name: "Sam Jones",
+                screenTime: [
+                    { date: "2019-06-11", usage: { mapMyRun: 2, whatsApp: 0, facebook: 0, safari: 10 } },
+                    { date: "2019-06-13", usage: { mapMyRun: 10, whatsApp: 20, facebook: 10, safari: 16 } },
+                    { date: "2019-06-14", usage: { mapMyRun: 42, whatsApp: 20, facebook: 40, safari: 31 } },
+                    { date: "2019-05-04", usage: { twitter: 10, instagram: 56, facebook: 61 } },
+                ]
+            },
+        ];
+        expect(getScreentimeAlertList(users, "2019-06-13")).toBe("0 results")
     })
 })
